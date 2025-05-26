98fcca59554b1ba0d0f78de858bab6717a6bd2eb
humantorch <kosman.scott@gmail.com>
2025-05-26 18:45:55 -0300

    fix for tailwind pathing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./frontend/index.html",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: ['bg-blue-100'],
 }
