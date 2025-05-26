4a483d82ea78665b614c5ea518d38cfea02c7315
humantorch <kosman.scott@gmail.com>
2025-05-26 18:49:10 -0300

    whoops tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 export default {
   content: [
-    "./frontend/index.html",
-    "./frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  safelist: ['bg-blue-100'],
+  safelist: ['bg-blue-100', 'outline', 'outline-blue-300'],
 }
